a03ab506c98f8a04aed23f7e6bddb1e71f93bcd3
skoddowl <skoddowl@gmail.com>
2015-01-30 22:59:57 +0200

    Added pretty comment.created_at formatting

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,11 @@
 module CommentsHelper
+  def format_date(date)
+    if date.to_date == Date.today
+      date.strftime("%H:%M")
+    elsif date.to_date == Date.yesterday
+      date.strftime("Вчера, %H:%M")
+    else
+      date.strftime("%H:%M %m.%d")
+    end
+  end
 end
