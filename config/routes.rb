db47fc07f05ccd748cd7ef2d4e64b1b9b68f748e
doroshenko-e <bekinassoff@gmail.com>
2015-01-31 10:10:41 +0200

    controller Messages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'messages/index'
+
+  get 'messages/show'
+
+  get 'messages/new'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'home#index'
