f1cc9fa805a4e1eef78e8a4619459caf9835e1f9
skoddowl <skoddowl@gmail.com>
2015-02-20 18:59:41 +0200

    Update js

---
+++ app/assets/javascripts/add_location.js
@@ -0,0 +1,33 @@
+function addLocation() {
+	var map = L.map('map').setView([49.437820, 32.058959], 13);
+
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        id: 'examples.map-i875mjb7'
+    }).addTo(map);
+
+    var marker;
+    if (presence == true) {
+        marker = addMarker(latlng[0], latlng[1]);
+    }
+
+    function onMapClick(e) {
+        if (marker != null) {
+            map.removeLayer(marker);
+        }
+        marker = addMarker(e.latlng.lat, e.latlng.lng);
+    }
+
+    map.on('click', onMapClick);
+
+    function addMarker(lat, lng) {
+        var new_marker = new L.Marker([lat, lng], { draggable:true });
+        map.addLayer(new_marker);
+        $.ajax({
+            url: "/get_location",
+            type: "post",
+            data : 'latitude=' + lat + '&longitude=' + lng
+        });
+        return new_marker;
+    }
+}
