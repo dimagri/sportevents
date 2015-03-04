febaf357ec4c1eb2ca84af28c898ea7585811498
skoddowl <skoddowl@gmail.com>
2015-03-04 19:00:09 +0200

    Fix db migrations

--- db/migrate/20150130145536_update.rb
+++ db/migrate/20150130145536_update.rb
@@ -1,6 +1,6 @@
 class Update < ActiveRecord::Migration
   def change
     remove_column :events, :date
-    add_column :events, :begins_at, :datetimerai
+    add_column :events, :begins_at, :datetime
   end
 end
