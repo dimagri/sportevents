fcc23adc02ec7123b563af32d71b7c338f06aa1b
skoddowl <skoddowl@gmail.com>
2015-03-04 12:28:43 +0200

    Updated event model in activeadmin

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,6 +1,12 @@
 ActiveAdmin.register Event do
 
   config.filters = false
+  config.per_page = 10
+
+  scope :not_started, default: true
+  scope :started
+
+  permit_params Event.column_names
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
