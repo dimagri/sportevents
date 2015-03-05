b63c09066e1889d209f038ac6cd9a9a222a0973e
skoddowl <skoddowl@gmail.com>
2015-03-05 21:52:05 +0200

    Fix omniauth for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # TODO - change website and callback URL to production URL's in dev center
   secrets = Rails.application.secrets
-  provider :twitter, secrets.twitter_app_id, secrets.twitter_secret_key
-  provider :facebook, secrets.facebook_app_id, secrets.facebook_secret_key
-  provider :vkontakte, secrets.vkontakte_app_id, secrets.vkontakte_secret_key
+  provider :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_SECRET_KEY']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET_KEY']
+  provider :vkontakte, ENV['VKONTAKTE_APP_ID'], ENV['VKONTAKTE_SECRET_KEY']
   provider :identity, on_failed_registration: lambda { |env|
     IdentitiesController.action(:new).call(env)
   }
