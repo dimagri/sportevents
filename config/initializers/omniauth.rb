7619bbae032906166cf51150c6806befce4799cb
skoddowl <skoddowl@gmail.com>
2015-03-07 15:45:30 +0200

    Remove facebook authorization

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   secrets = Rails.application.secrets
   provider :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_SECRET_KEY']
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET_KEY']
   provider :vkontakte, ENV['VKONTAKTE_APP_ID'], ENV['VKONTAKTE_SECRET_KEY']
   provider :identity, on_failed_registration: lambda { |env|
     IdentitiesController.action(:new).call(env)
