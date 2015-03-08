ced9356c81b1478cd70e88f609c9b2e755c3d246
skoddowl <skoddowl@gmail.com>
2015-03-08 12:32:39 +0200

    Use Sendgrid for mailing

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "gmail.com",
-    :user_name            => "sportevents.ck@gmail.com",
-    :password             => "qsKXkQ8iFj",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
